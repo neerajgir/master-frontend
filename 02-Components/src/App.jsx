88e788e31e8675707e0474c7467e60936fb6c472
Neeraj Gir <neerajgoswami871@gmail.com>
2026-09-02 02:34:08 +0500

    feat: add Header and Hero components, implement UI elements (Button, Card, Input) and utility functions
    - Created Header component with navigation links and statistics display.
    - Developed Hero component with a promotional section and mobile menu functionality.
    - Implemented reusable UI components: Button, Card (with CardHeader, CardTitle, CardDescription, CardContent, CardFooter), and Input.
    - Added utility function for class name merging.
    - Configured TypeScript settings for the application and node environments.

--- 02-Components/src/App.jsx
+++ 02-Components/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Card from './components/Card.jsx'
-
+import Header from './components/Header.jsx'
+// import Hero from './components/Hero.jsx'
 const App = () => {
   return (
+
     <div className="font-bold bg-amber-300 text-blue-900 border-2 p-4">
     <h1 className="text-emerald-600 dark:text-sky-600 border-2 p-4 rounded-xl">Welcome To Components</h1>
+    {/* <Hero/> */}
+    <Header/>
     <div className="flex gap-4">
       <Card imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1280px-Python-logo-notext.svg.png?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=thumbnail" title="Buy Python" description="Python is a programming language that lets you work more quickly and integrate your systems more effectively."/>
       <Card imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi7-QSbrTDw-udyB0dgf9D0-vdxKg8GC8wG3v-c3XdeA&s" title="Buy JavaScript" description="JavaScript is a programming language that conforms to the ECMAScript specification, which was created to standardize JavaScript"/>
--- 02-Components/vite.config.js
+++ 02-Components/vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,4 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
