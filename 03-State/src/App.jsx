6804f056c105581c95e815e6584e6f7a1806c1ef
Neeraj Gir <neerajgoswami871@gmail.com>
2026-09-03 00:15:59 +0500

    feat: implement counter functionality with increase, decrease, and reset buttons; add card styling

--- 03-State/src/App.jsx
+++ 03-State/src/App.jsx
@@ -1,121 +1,35 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
-
+  const [input, setInput] = useState(0)
+  // const increase = () => {
+  //   setCount(count + 1)
+  // }
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
+    <div>
+      <h1>Counter</h1>
+      <div className="card">Count is {count}</div>
+      <div>
         <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+        onClick={()=>setCount(count + 1)}
+        style={{margin:"0 5px"}}>Increase</button>
+        <button
+        onClick={()=>setCount((count)=>Math.max(count - 1, 0))}
+        style={{margin:"0 5px"}}>Decrease</button>
+        <button
+        onClick={()=>setCount(0)}
+        style={{margin:"0 5px"}}>Reset</button>
+      </div>
+      <div style={{margin: "20px 0"}}>
+        <input style={{width: "100px", border:"1px solid white", margin: "0 5px", padding:"0.6em 1.2em"}} 
+        value={input}
+        onChange={(e)=> setInput(Number(e.target.value))}
+        type="text" />
+        <button style={{margin: "0 5px", }} onClick={()=>{setCount(Number(input)), setInput(0)}}>Set to {input}</button>
+      </div>
+    </div>
   )
 }
 
