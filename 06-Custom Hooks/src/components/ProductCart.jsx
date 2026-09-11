319a90e2f2d94c1306b3b1c9fb393b0ca73bd06d
Neeraj Gir <neerajgoswami871@gmail.com>
2026-09-12 01:29:49 +0500

    feat: add ProductCart component and product data; implement useCart hook for cart management

--- 06-Custom Hooks/src/components/ProductCart.jsx
+++ 06-Custom Hooks/src/components/ProductCart.jsx
@@ -1,7 +1,14 @@
+import {FaShoppingCart} from 'react-icons/fa'
 
-const ProductCart = () => {
+const ProductCart = ({product, onAddToCart}) => {
   return (
-    <div>ProductCart</div>
+    <div>
+      <h3>{product.name}</h3>
+      <p>${product.price}</p>
+      <button onClick={() => onAddToCart(product)}>
+        <FaShoppingCart/> Add to Cart
+      </button>
+    </div>
   )
 }
 
