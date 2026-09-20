52c94ecbeffc65adcc5250dc9198209f4af672ac
Neeraj Gir <neerajgoswami871@gmail.com>
2026-09-20 13:45:20 +0500

    client and server components

--- Nextjs/04-rendering-components-deep-dive/app/page.js
+++ Nextjs/04-rendering-components-deep-dive/app/page.js
@@ -1,17 +1,18 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
 
-const HomePage = async() => {
-  const data  = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const res = await data.json()
-  console.log(res)
+
+const HomePage = () => {
+  const [name, setName] = useState("Neeraj")
   return (
     <div>
-      <button >
-        Click Me
-        {JSON.stringify(res)}
+      {name}
+      <button onClick={() => setName("Neeraj Kumar")}>
+        change name
       </button>
     </div>
   )
 }
+
 
 export default HomePage
---
+++ Nextjs/04-rendering-components-deep-dive/app/server.js
@@ -0,0 +1,13 @@
+// const HomePage = async() => {
+//   const data  = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   const res = await data.json()
+//   console.log(res)
+//   return (
+//     <div>
+//       <button >
+//         Click Me
+//         {JSON.stringify(res)}
+//       </button>
+//     </div>
+//   )
+// }
